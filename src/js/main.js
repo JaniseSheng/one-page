cca0eff714a2c8b607eb5cb106a98a6d33c88f37
shengxx <janisesheng@cform.io>
2016-11-27 00:13:38 +0800

    add loading add can

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 
 $(document).ready(function () {
     console.log("我的第一个jQuery代码!");
+
+    $('body').imagesLoaded( function() {
+        $('.loading').remove();
+    });
     var swiper = new Swiper('.swiper-container', {
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
